6bc842787ecd22eed89d2476ff68ad932dc357a1
Edinardo Freitas Filho <edinardofreitas@gmail.com>
2020-08-17 15:47:04 -0300

    Category

---
+++ src/main/java/com/edinardofilho/course/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.edinardofilho.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edinardofilho.course.entities.Category;
+import com.edinardofilho.course.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
