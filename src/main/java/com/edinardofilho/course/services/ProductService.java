c50319e95d50f958b71bebb1efa032e20875aeb6
Edinardo Freitas Filho <edinardofreitas@gmail.com>
2020-08-17 16:11:22 -0300

    Product

---
+++ src/main/java/com/edinardofilho/course/services/ProductService.java
@@ -0,0 +1,26 @@
+package com.edinardofilho.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edinardofilho.course.entities.Product;
+import com.edinardofilho.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll() {
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+}
